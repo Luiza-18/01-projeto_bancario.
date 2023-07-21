467cde2847fb037bfef5970682180211028899b6
Luiza <140194852+Luiza-18@users.noreply.github.com>
2023-07-21 16:31:32 -0300

    Add files via upload

---
+++ projeto_bancario_v1.js
@@ -0,0 +1,63 @@
+const limiteSaques = 3;
+
+let saldo = 0;
+let limite = 500;
+let extrato = "";
+let numeroSaques = 0;
+
+
+while(true) {
+    menu = prompt(`
+    ======== M E N U ========
+    [d] Depósito
+    [s] Saque
+    [e] Extrato
+    [q] Sair
+    =========================
+    `);
+
+    if(menu == "d") {
+       valor = parseFloat(prompt("Informe o valor do depósito: R$ "));
+    
+    if (valor > 0) {
+        saldo += valor 
+        extrato += `Depósito: R$${valor} \n`;
+        alert(`Depósito de R$${valor} realizado.`);
+   }  else{
+      alert("Operação falhou! O valor informado é inválido!")
+   }
+   } else if(menu == "s") {
+      valor = parseFloat(prompt("Informe o valor para saque: "));
+      excedeuSaldo = valor > saldo;
+      excedeuLimite = valor > limite;
+      excedeuSaque = numeroSaques >= limiteSaques;
+
+    if (excedeuSaldo) {
+       alert("Operação falhou! Você não tem saldo suficiente!");
+   } else if(excedeuLimite) {
+       alert("Operação falhou! O valor do saque excede o limite!");
+   } else if(excedeuSaque) {
+       alert("Operação falhou! Número máximo de saques excedido!");
+   } else if (valor > 0) {
+       saldo -= valor;
+       extrato += `Saque de R$${valor} \n`;
+       alert(`Saque de R$${valor} realizado.`);
+       numeroSaques+=1;
+   }  else{
+       alert("Operação falhou! O valor informado é inválido!");
+        }
+}  else if(menu == "e") {
+   mensagem = extrato == true ? "Não foram realizadas movimentações" : extrato
+   alert(`
+   =========== E X T R A T O ==============
+   ${mensagem}
+   Saldo: R$ ${saldo.toFixed(2)}
+   ====================================
+   `)
+}  else if(menu == "q") {
+   alert("Obrigado pela preferência !!!")
+   break
+}  else {
+   "Opção inválida, por favor selecione novamente a operação desejada! ";
+}
+}
